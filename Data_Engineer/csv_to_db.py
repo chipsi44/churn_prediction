291d09cc75175100aaf6fe229894d14aca12afdf
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-02-20 10:33:18 +0100

    DB CREATED

--- Data_Engineer/csv_to_db.py
+++ Data_Engineer/csv_to_db.py
@@ -1,39 +1,7 @@
-import csv
+import pandas as pd
 import sqlite3
-
-conn = sqlite3.connect('BankChurners.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-CREATE TABLE client 
-(
-    CLIENTNUM INTEGER PRIMARY KEY,
-    Attrition_Flag TEXT,
-    Customer_Age INTEGER,
-    Gender TEXT,
-    Dependent_count INTEGER,
-    Education_Level TEXT,
-    Marital_Status TEXT,
-    Income_Category TEXT,
-    Card_Category TEXT,
-    Months_on_book INTEGER,
-    Total_Relationship_Count INTEGER,
-    Months_Inactive_12_mon INTEGER,
-    Contacts_Count_12_mon INTEGER,
-    Credit_Limit INTEGER,
-    Total_Revolving_Bal INTEGER,
-    Avg_Open_To_Buy INTEGER,
-    Total_Amt_Chng_Q4_Q1 FLOAT,
-    Total_Trans_Amt INTEGER,
-    Total_Trans_Ct INTEGER,
-    Total_Ct_Chng_Q4_Q1 FLOAT,
-    Avg_Utilization_Ratio FLOAT ,
-    Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1 FLOAT,
-    Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2 FLOAT
-)
-''')
-
-
-
-conn.commit()
-conn.close()
+def csv_to_db(csv_file,db_name) :
+    df = pd.read_csv(csv_file)
+    conn = sqlite3.connect(db_name)
+    df.to_sql('client', conn, if_exists='replace', index=False)
+    conn.close()
