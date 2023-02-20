d2f41782b45f6ebfbe695818181559a44397aa71
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-02-20 16:41:24 +0100

    End of the day

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template,request
+
 app = Flask(__name__)
 
-# defines a route that is accessible at the root URL ("/").
 @app.route("/", methods=["GET", "POST"])
 # This function is executed when a request is made to the root URL.
 def predict():
@@ -9,5 +9,9 @@ def predict():
         # If the request method is "GET", the string "ALIVE" is printed and the "index.html" template is rendered.
         print("ALIVE")
         return render_template("index.html")
-        
+    
+    else:
+        print("From Post") 
+        return render_template("index.html")
+
 app.run(debug=False)
