a111580a494f6896d70c73cf77fc56f676000d5a
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-02-23 15:30:31 +0100

    He's alive !

--- app.py
+++ app.py
@@ -1,22 +1,45 @@
-from flask import Flask, render_template,request
+# Import required libraries
+from flask import Flask, render_template, request
 import pandas as pd
+import pickle
 
-
+# Initialize the Flask app
 app = Flask(__name__)
 
+# Define the route for the root URL
 @app.route("/", methods=["GET", "POST"])
-# This function is executed when a request is made to the root URL.
 def predict():
+    # Check the request method
     if request.method == "GET":
-        # If the request method is "GET", the string "ALIVE" is printed and the "index.html" template is rendered.
+        # If the request method is GET, return the index.html template
         print("ALIVE")
         return render_template("index.html")
     else:
-        print("From Post") 
-        list_val = []
-        for key,value in request.form.items() :
-            list_val.append(value)
-        value_to_predict = pd.Series(list_val)
-        return render_template("index.html", prediction = "waiting for ML", args = list_val)
+        # If the request method is POST, load the pre-trained model and use it to make a prediction
+        file_pckl = open('Classification_model_pipeline.pkl', 'rb')
+        model = pickle.load(file_pckl)
 
+        # Get the user inputs from the form and create a dictionary of values
+        dic_val = {}
+        list_val = []
+        for key, value in request.form.items():
+            list_val.append(value)
+            dic_val[key] = value
+        
+        # Convert the dictionary of values to a Pandas DataFrame
+        value_to_predict = pd.DataFrame([dic_val])
+        
+        # Use the pre-trained model to make a prediction
+        reponse = model.predict(value_to_predict)
+
+        # Convert the prediction to a human-readable string and return it to the user
+        if reponse[0] == 1: 
+            reponse = 'Customer may churn'
+        else: 
+            reponse = "Customer is not likely to churn"
+        
+        # Return the index.html template with the prediction and user inputs
+        return render_template("index.html", prediction=reponse, args=list_val)
+
+# Start the Flask app
 app.run(debug=False)
