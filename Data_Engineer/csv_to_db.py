fcba4e38f063d1af00b2715e4149a7b3ed7bceb2
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-02-24 10:41:48 +0100

    deleted

--- Data_Engineer/csv_to_db.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import sqlite3
-
-def csv_to_db(csv_file,db_name) :
-    df = pd.read_csv(csv_file)
-    conn = sqlite3.connect(db_name)
-    df.to_sql('client', conn, if_exists='replace', index=False)
-    conn.close()
