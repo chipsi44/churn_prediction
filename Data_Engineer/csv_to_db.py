a9bc1ae25f8f3f00afe0ca0fe9d699d8faff798a
Verwimp Cyril <119902908+chipsi44@users.noreply.github.com>
2023-02-27 10:17:49 +0100

    new DB

--- Data_Engineer/csv_to_db.py
+++ Data_Engineer/csv_to_db.py
@@ -4,5 +4,7 @@ import sqlite3
 def csv_to_db(csv_file,db_name) :
     df = pd.read_csv(csv_file)
     conn = sqlite3.connect(db_name)
-    df.to_sql('client', conn, if_exists='replace', index=False)
+    df.to_sql('cluster', conn, if_exists='replace', index=False)
     conn.close()
+
+csv_to_db('BankChurnerswithCluster.csv','BankChurnerswithCluster.db')
